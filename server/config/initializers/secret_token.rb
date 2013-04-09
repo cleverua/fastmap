e11ad5cd968b9ea60c9d35e50072b22e9b8ef6a8
Andrew Koniushiy <andrew@cleverua.com>
2013-04-09 11:27:40 +0300

    init commit

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '020b334d818af4f121a96d6bffa616eb149fa68a82d80c7a0cd5ad0f0af155af8551243f8865ea9f11b9737872725417ce542d16d138fbc134ce8a531fbaff1e'
