7041a6ad278bdc98c52086ded7602c3deeb92a72
Andrew Koniushiy <andrew@cleverua.com>
2013-04-10 15:17:06 +0300

    fix unicorn config

--- server/config/unicorn.rb
+++ server/config/unicorn.rb
@@ -33,13 +33,13 @@ listen "/tmp/unicorn.sock", :backlog => 64
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "#{app_path}/shared/pids/unicorn.pid"
+pid "#{app_path}/tmp/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
